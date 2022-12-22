5ff83990c1ea6239683795ce38a29c38f5af1939
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2022-12-22 02:42:04 -0300

    fix: removed console.log

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,20 +10,17 @@ export async function signUp(req, res) {
 		await createUser({ ...user, password: hashPassword });
 		res.sendStatus(201);
 	} catch (error) {
-		console.log(error);
 		res.sendStatus(500);
 	}
 }
 
 export async function signIn(req, res) {
 	const user = req.locals.user;
-	console.log("🚀 ~ file: usersController.js:20 ~ signIn ~ user", user)
 	const token = jwt.sign({ id: user.id }, process.env.SECRET_JWT, { expiresIn: 86400 });
 
 	try {
 		res.send({ ...user, token: token });
 	} catch (error) {
-		console.log(error);
 		res.sendStatus(500);
 	}
 }
